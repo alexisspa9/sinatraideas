e5555084025fd53b2fa735436c7b7187f7f87e74
Alexander <alexisspa9@gmail.com>
2018-04-03 11:51:25 +0300

    completed controller actions for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -136,18 +136,56 @@ get '/myideas/:id' do
   end
 
 
+  get '/users/:id' do
+    @user = User.find_by_id(params[:id])
+    erb :'users/show'
+  end
 
 
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+    else
+      redirect to '/myideas'
+    end
+  end
 
+  post '/signup' do
+    if params[:name] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:name => params[:name], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/myideas'
+    end
+  end
 
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect '/myideas'
+    end
+  end
 
+  post '/login' do
+    user = User.find_by(:name => params[:name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/myideas"
+    else
+      redirect to '/signup'
+    end
+  end
 
-
-
-
-
-
-
-
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 
 end
